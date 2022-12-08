061dd151559ef9bee0427d4a7e5e7b0af6416841
rae-t627 <105154462+rae-t627@users.noreply.github.com>
2022-12-08 19:07:00 +0530

    Add files via upload

---
+++ Beginner Level/BIRIYANI/code.cpp
@@ -0,0 +1,17 @@
+//BIRIYANI
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test_cases;
+	int x;
+	int y;
+	
+	cin >> test_cases;
+	
+	for (int i = 0; i < test_cases; i ++){
+	    cin >> x >> y;
+	    cout << x * y << endl;
+	}
+	return 0;
+}

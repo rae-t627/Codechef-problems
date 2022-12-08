cba2f96208826d4e86e13e790148a4093499b1f5
rae-t627 <105154462+rae-t627@users.noreply.github.com>
2022-12-08 19:02:24 +0530

    Add files via upload

---
+++ Beginner Level/SQUATS/code.cpp
@@ -0,0 +1,16 @@
+//SQUATS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test_cases;
+	int x;
+	
+	cin >> test_cases;
+	
+	for (int i = 0; i < test_cases; i ++){
+	    cin >> x;
+	    cout << x*15 << endl;
+	}
+	return 0;
+}
